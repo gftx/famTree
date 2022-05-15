b91e24a2ebc5c7feae62f6fc2f1167533ab6bdd5
Anton Ivanov <ivanovdevelop@yandex.ru>
2022-05-15 12:51:11 +0300

    add put and delete person

--- server/routes/person.ts
+++ server/routes/person.ts
@@ -17,7 +17,6 @@ router.get('/persons', (request: Request, response: Response) => {
     })
 })
 
-
 router.post('/persons', (request: Request, response: Response) => {
     const person: IPerson = request.body;
     const {
@@ -31,10 +30,7 @@ router.post('/persons', (request: Request, response: Response) => {
         sistersIds = null,
         childrenIds = null
     } = person
-    console.log(typeof name, `${name}`)
 
-
-    // language=SQL format=true
     pool.query(`INSERT INTO public.person (name, surname, birthdate, image, father_id, mother_id, brothers, sisters, children)
         VALUES (\'${name}\', \'${surname}\', \'${birthdate}\', \'${image}\', \'${fatherId}\',
         \'${motherId}\', \'${brothersIds}\', \'${sistersIds}\', \'${childrenIds}\');`,
@@ -45,10 +41,62 @@ router.post('/persons', (request: Request, response: Response) => {
                     message: err.stack
                 })
             }
+            return response.send({
+                message: 'человек успешно создан',
+            });
+        }
+    )
+});
+
+router.put('/persons', (request: Request, response: Response) => {
+    const person: IPerson = request.body;
+    const {
+        id,
+        name,
+        surname,
+        birthdate,
+        image,
+        fatherId,
+        motherId,
+        brothersIds,
+        sistersIds,
+        childrenIds
+    } = person
+
+    pool.query(`UPDATE public.person SET "name"=\'${name}\', "surname"=\'${surname}\', "birthdate"=\'${birthdate}\', 
+                "image"=\'${image}\', "father_id"=\'${fatherId}\', "mother_id"=\'${motherId}\', "brothers"=\'${brothersIds}\',
+                "sisters"=\'${sistersIds}\', "children"=\'${childrenIds}\'  WHERE "id"=\'${id}\';`,
+        (err: Error, res: QueryResult) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                return response.send({
+                    message: err.stack
+                })
+            }
             console.log('post response', res)
 
             return response.send({
-                message: 'человек успешно создан',
+                message: 'человек успешно обновлен',
+            });
+        }
+    )
+});
+
+
+router.delete('/persons', (request: Request, response: Response) => {
+    const { id } = request.body
+
+    pool.query(`DELETE from public.person WHERE id=${id}`,
+        (err: Error, res: QueryResult) => {
+            if (err) {
+                console.error('Error executing query', err.stack);
+                return response.send({
+                    message: err.stack
+                })
+            }
+
+            return response.send({
+                message: 'человек успешно удален',
             });
         }
     )
